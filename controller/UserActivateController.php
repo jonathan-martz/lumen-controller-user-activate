081c42616d88651dc2489a00c403408ef4056840
jonathan-martz <github@jmartz.de>
2019-12-27 22:15:51 +0100

    add logic for user activate controller

--- controller/UserActivateController.php
+++ controller/UserActivateController.php
@@ -14,7 +14,43 @@ class UserActivateController extends Controller
      */
     public function activate()
     {
-        $validation = $this->validate($this->request, []);
+        $validation = $this->validate($this->request, [
+            'token' => 'required|string|size:512'
+        ]);
+
+        $tokens = DB::table('user_activate_token')->where('token', '=', $this->request->input('token'));
+
+        if ($tokens->count() === 1) {
+            $token = $tokens->get()->first();
+            $this->addMessage('success', 'Token exists.');
+
+            if (!empty($token->userid)) {
+                $count = DB::table('users')->where('id', '=', $token->userid)->count();
+                $active = DB::table('users')->where('id', '=', $token->userid)->where('active', '=', '0')->count();
+                if ($count === 1) {
+                    if ($active === 1) {
+                        $update = DB::table('users')->where('id', '=', $token->userid)->update([
+                            'active' => '1'
+                        ]);
+
+                        if ($update) {
+                            $this->addMessage('success', 'User activated now.');
+                        } else {
+                            $this->addMessage('error', 'Cant update user.');
+                        }
+                    } else {
+                        $this->addMessage('error', 'User already activated.');
+                    }
+
+                } else {
+                    $this->addMessage('error', 'User doesnt exists.');
+                }
+            } else {
+                $this->addMessage('error', 'User id empty.');
+            }
+        } else {
+            $this->addMessage('error', 'Token doesnt exists.');
+        }
 
         return $this->getResponse();
     }
